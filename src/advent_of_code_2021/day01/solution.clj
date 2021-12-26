d0612ef640edb3c404c3219f5c4fd1d75d46cd8e
Sheng Yunzhou <yunzhou@inspify.com>
2021-12-26 18:02:48 +0800

    initial commit of day01 solution

---
+++ src/advent_of_code_2021/day01/solution.clj
@@ -0,0 +1,53 @@
+(ns advent-of-code-2021.day01
+  (:require [clojure.java.io :as io]))
+
+(def sample-input [199
+                   200
+                   208
+                   210
+                   200
+                   207
+                   240
+                   269
+                   260
+                   263])
+
+(def data-file (io/resource
+                "day01/part_one.txt"))
+
+(def part-one-input (->> data-file
+                         slurp
+                         clojure.string/split-lines
+                         (map #(Integer/parseInt %))))
+
+(defn part-one-solution [input]
+  (let [interleaved (interleave input (drop 1 input))]
+    (->> interleaved
+         (partition 2)
+         (map #(if (> (second %) (first %)) 1 0))
+         (reduce +))))
+
+(part-one-solution part-one-input)
+
+(defn part-two-solution [input]
+  (let [parsed (interleave input (drop 1 input) (drop 2 input))]
+    (->> parsed
+         (partition 3)
+         (map #(apply + %))
+         part-one-solution)))
+
+(part-two-solution sample-input)
+(part-two-solution part-one-input)
+
+(comment
+  part-one-input
+  (part-one-solution sample-input)
+
+  (->> sample-input
+       (map inc)
+       (reduce +))
+  (part-one-solution (map #(apply + %) 
+       (partition 3 
+                  (interleave sample-input 
+                              (drop 1 sample-input) 
+                              (drop 2 sample-input))))))
