385fbaf7211314e6bc240d1086f77de8a739f851
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-01-01 14:00:49 +0800

    Add solution to day07 part 2

--- src/day07/solution.clj
+++ src/day07/solution.clj
@@ -8,11 +8,14 @@
 (defn parse-input [input]
   (map #(Integer/parseInt %) (string/split input #",")))
 
-(parse-input sample-input)
-
 (defn get-distance [parsed-input n]
   (->> parsed-input
        (map #(Math/abs (- n %)))
+       (reduce +)))
+
+(defn get-distance-stepped [parsed-input n]
+  (->> parsed-input
+       (map #(reduce + (range (inc (Math/abs (- n %))))))
        (reduce +)))
 
 (let [max-input (apply max (parse-input sample-input))
@@ -29,5 +32,20 @@
          (map #(get-distance parsed-input %))
          (apply min))))
 
+(defn part-two-solution [input]
+  (let [parsed-input (parse-input input)
+        max-input (apply max parsed-input)
+        min-input (apply min parsed-input)]
+    (->> (range min-input (inc max-input))
+         (map #(get-distance-stepped parsed-input %))
+         (apply min))))
+
+(comment
+  (parse-input sample-input)
+  (get-distance-stepped (parse-input sample-input) 2))
+
 (part-one-solution sample-input)
 (part-one-solution part-one-input)
+
+(part-two-solution sample-input)
+(part-two-solution part-one-input) ;; quite slow, may need to convert input into frequencies and only calculate for distinct values
