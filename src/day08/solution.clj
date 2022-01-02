31145ceed778cc6597f7dbf61a6baba1120f4dbc
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-01-02 16:12:02 +0800

    Add solution to day08 part two

--- src/day08/solution.clj
+++ src/day08/solution.clj
@@ -45,3 +45,72 @@ part-one-input
 
 (part-one-solution sample-input)
 (part-one-solution part-one-input)
+
+(defn find-three [len-five-segments one]
+  (first (filter #(= 2 (count (clojure.set/intersection
+                               (set one)
+                               (set %)))) len-five-segments)))
+
+(defn find-two [two-or-five four]
+  (first (filter #(= 2 (count (clojure.set/intersection
+                               (set four)
+                               (set %)))) two-or-five)))
+
+(defn sort-alphabetically [segments]
+  (map string/join (map sort (map #(string/split % #"") segments))))
+
+(defn find-nine [len-six-segments four]
+  (first (filter #(= 0 (count (clojure.set/difference
+                               (set four)
+                               (set %)))) len-six-segments)))
+
+(defn find-zero [zero-or-six one]
+  (first (filter #(= 0 (count (clojure.set/difference
+                               (set one)
+                               (set %)))) zero-or-six)))
+
+(defn decode-input-entry [input-entry]
+  (let [segments-by-len (group-by count (sort-alphabetically input-entry))
+        one (first (get segments-by-len 2))
+        four (first (get segments-by-len 4))
+        seven (first (get segments-by-len 3))
+        eight (first (get segments-by-len 7))
+        len-five-segments (set (get segments-by-len 5))
+        three (find-three len-five-segments one)
+        two-or-five (set (remove #{three} len-five-segments))
+        two (find-two two-or-five four)
+        five (first (remove #{two} two-or-five))
+        len-six-segments (set (get segments-by-len 6))
+        nine (find-nine len-six-segments four)
+        zero-or-six (set (remove #{nine} len-six-segments))
+        zero (find-zero zero-or-six one)
+        six (first (remove #{zero} zero-or-six))]
+    {zero 0
+     one 1
+     two 2
+     three 3
+     four 4
+     five 5
+     six 6
+     seven 7
+     eight 8
+     nine 9}))
+
+(defn get-output-value [input output]
+  (let [sorted-output (sort-alphabetically output)]
+    (Integer/parseInt (string/join (map #(get (decode-input-entry input) %) sorted-output)))))
+
+(defn get-output-value-by-line [line]
+  (let [[i o] (string/split line #"\s\|\s")
+        input (string/split i #"\s")
+        output (string/split o #"\s")]
+    (get-output-value input output)))
+
+(defn part-two-solution [input]
+  (->> input
+       string/split-lines
+       (map get-output-value-by-line)
+       (reduce +)))
+
+(part-two-solution sample-input)
+(part-two-solution part-one-input)
