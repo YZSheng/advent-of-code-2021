422515af0d4319b44a33e6bfe803d95cb042ce6e
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-01-16 16:52:30 +0800

    Add day12 part two solution

--- src/day12/solution.clj
+++ src/day12/solution.clj
@@ -31,9 +31,29 @@ b-end")
        (remove #(and (not (is-upper-case %)) (some #{%} path)))
        (map #(conj path %))))
 
+(defn can-add-node? [path node]
+  (let [added (conj path node)]
+    (if (= "start" node)
+      false
+      (->> added
+           (remove is-upper-case)
+           frequencies
+           vals
+           (filter #(> % 1))
+           (#(or (= '(2) %) (empty? %)))))))
+
+(defn extend-path-allow-single-small-cave [path nodes]
+  (->> nodes
+       (remove #(and (not (is-upper-case %)) (not (can-add-node? path %))))
+       (map #(conj path %))))
+
 (defn get-possible-extended-paths [path input]
   (let [next-nodes (get-next-nodes (last path) input)]
     (extend-path path next-nodes)))
+
+(defn get-possible-extended-paths-part-two [path input]
+  (let [next-nodes (get-next-nodes (last path) input)]
+    (extend-path-allow-single-small-cave path next-nodes)))
 
 (defn get-potential-paths [paths]
   (->> paths
@@ -41,7 +61,6 @@ b-end")
 
 (defn is-valid-path [path]
   (= "end" (last path)))
-
 
 (defn find-all-paths [input]
   (loop [paths [["start"]]
@@ -51,11 +70,26 @@ b-end")
         valid-paths
         (recur extended-paths (concat valid-paths (filter is-valid-path extended-paths)))))))
 
+(defn find-all-paths-part-two [input]
+  (loop [paths [["start"]]
+         valid-paths []]
+    (let [extended-paths (mapcat #(get-possible-extended-paths-part-two % input) paths)]
+      (if (empty? extended-paths)
+        valid-paths
+        (recur extended-paths (concat valid-paths (filter is-valid-path extended-paths)))))))
+
 (defn part-one-solution [input]
   (count (find-all-paths input)))
 
+(defn part-two-solution [input]
+  (count (find-all-paths-part-two input)))
+
+
 (part-one-solution sample-input)
 (part-one-solution part-one-input)
+
+(part-two-solution sample-input)
+(part-two-solution part-one-input)
 
 (comment
 
@@ -71,8 +105,19 @@ b-end")
   (get-possible-extended-paths ["start" "A" "end"] sample-input)
   (get-possible-extended-paths ["start" "b" "d"] sample-input)
 
+  (get-possible-extended-paths-part-two ["start" "b" "d"] sample-input)
+
+  (can-add-node? ["a" "b" "c" "b"] "b")
+  (can-add-node? ["a" "b" "c" "b"] "d")
+  (can-add-node? ["a" "b" "c" "b"] "start")
+  (can-add-node? ["a" "b" "c" "b" "A" "A"] "s")
+
   (let [paths [["start" "A" "c" "A" "b" "A" "end"] ["start" "A" "b" "A" "c" "A" "end"]]
         extended-paths (mapcat #(get-possible-extended-paths % sample-input) paths)]
+    extended-paths)
+
+  (let [paths [["start" "A" "c" "A"] ["start" "A" "b" "A"] ["start" "A" "b" "d"] ["start" "A" "b" "end"] ["start" "b" "A" "c"] ["start" "b" "A" "b"] ["start" "b" "A" "end"] ["start" "b" "d" "b"]]
+        extended-paths (mapcat #(get-possible-extended-paths-part-two % sample-input) paths)]
     extended-paths)
 
   (loop [paths [["start"]]
